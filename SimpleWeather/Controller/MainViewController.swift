1484918c8fe81a58f120a44b1989a970f4319ce8
alexbusol <ovbusol@crimson.ua.edu>
2018-07-16 16:26:32 +0300

    Implemented Core Location locationManager method to receive the device's current location.

--- SimpleWeather/Controller/MainViewController.swift
+++ SimpleWeather/Controller/MainViewController.swift
@@ -17,6 +17,7 @@ class MainViewController: UIViewController, CLLocationManagerDelegate {
     let APP_ID = "6eadaf273a536bfe04601969558990de"
     
     let locationManager = CLLocationManager()
+    let weatherDataModel = WeatherDataModel()
 
     @IBOutlet weak var weatherImage: UIImageView!
     @IBOutlet weak var cityName: UILabel!
@@ -37,6 +38,35 @@ class MainViewController: UIViewController, CLLocationManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    //MARK: - Gettting the location via location manager
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1] //getting the most recent location.
+        
+        if location.horizontalAccuracy > 0 { //making sure that the value we are getting is not invalid.
+            locationManager.stopUpdatingLocation()
+            print("longitude: \(location.coordinate.longitude)", "latitude: \(location.coordinate.latitude)" )
+            
+            let latitude = String(location.coordinate.latitude)
+            let longitude = String(location.coordinate.longitude) //converting to string for the api call.
+            
+            let params : [String : String] = ["lat":latitude, "lon":longitude, "appid":APP_ID]
+            
+            getWeatherData(url: WEATHER_URL, parameters: params)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityName.text = "Location Error"
+    }
+    
+    //making a request to the API to get the weather data based on our location
+    func getWeatherData(url: String, parameters: [String: String]) {
+        
+    }
+    
 
 }
 
